50e031d9a8aca3e2be31bbd75e3535444cf230bd
geversde <43898281+geversde@users.noreply.github.com>
2023-05-19 17:49:07 -0700

    Update mlbrhodashapp.py

--- mlbrhodashapp.py
+++ mlbrhodashapp.py
@@ -3,7 +3,7 @@ from datetime import datetime, date
 import pandas as pd
 app = Dash(__name__)
 lnk = 'https://raw.githubusercontent.com/geversde/python-github-action-template/main/rho_output/mlbrho' + datetime.today().strftime("%Y%m%d") +'.csv'
-
+server = app.server
 
 def serve_layout():
     return dash_table.DataTable(data = pd.read_csv(lnk).to_dict("records"),page_size=15)
