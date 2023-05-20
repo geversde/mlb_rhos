b1560a3f4265a28697f5130dc1f577a37f9e2291
geversde <43898281+geversde@users.noreply.github.com>
2023-05-19 17:42:38 -0700

    Add files via upload

---
+++ mlbrhodashapp.py
@@ -0,0 +1,15 @@
+from dash import Dash, html, dash_table
+from datetime import datetime, date
+import pandas as pd
+app = Dash(__name__)
+lnk = 'https://raw.githubusercontent.com/geversde/python-github-action-template/main/rho_output/mlbrho' + datetime.today().strftime("%Y%m%d") +'.csv'
+
+
+def serve_layout():
+    return dash_table.DataTable(data = pd.read_csv(lnk).to_dict("records"),page_size=15)
+
+app.layout = serve_layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
